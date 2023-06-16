d7abeee028f311d796390747d00d02055f3da61e
EdsonGeller <edsongellerdiadasrosas@gmail.comr>
2023-06-16 12:35:32 -0300

    Adicionando EndPoint All

--- src/main/java/br/com/dicadeumdev/API/services/userServices.java
+++ src/main/java/br/com/dicadeumdev/API/services/userServices.java
@@ -1,7 +1,10 @@
 package br.com.dicadeumdev.API.services;
 
+import java.util.List;
+
 import br.com.dicadeumdev.API.domain.User;
 
 public interface UserServices {
     User findById(Integer id);
+    List<User> findAll();
 }
