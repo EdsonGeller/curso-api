c5fb139eebdd372a8101a63f42a8cc8d94f2c4f5
EdsonGeller <edsongellerdiadasrosas@gmail.comr>
2023-06-16 04:22:30 -0300

    Atualizando pom

--- src/main/java/br/com/dicadeumdev/API/resources/UserResource.java
+++ src/main/java/br/com/dicadeumdev/API/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.dicadeumdev.API.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.dicadeumdev.API.domain.User;
+import br.com.dicadeumdev.API.domain.DTO.UserDTO;
 import br.com.dicadeumdev.API.services.UserServices;
 
 @RestController
@@ -15,11 +16,15 @@ import br.com.dicadeumdev.API.services.UserServices;
 public class UserResource {
     
     @Autowired
+    private ModelMapper mapper;
+
+
+    @Autowired
     private UserServices service;
 
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
